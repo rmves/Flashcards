6cb8a838e90eb4379bf6f96f6af50048f3911601
rmves <rvesleda@gmail.com>
2023-12-24 00:45:19 +0000

    Create database manager class

---
+++ Flashcards/DatabaseManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DatabaseManager
+{
+    private string connectionString;
+
+    public DatabaseManager(string dbConnectionString)
+    {
+        connectionString = dbConnectionString;
+    }
+
+    public void CreateDatabaseAndTables()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to SQL Server.");
+
+                // Create the database
+                CreateDatabase(connection);
+
+                // Use the newly created database
+                connection.ChangeDatabase("FlashcardApp");
+
+                // Create tables if they don't exist
+                CreateTableStacks(connection);
+                CreateTableFlashcards(connection);
+
+                Console.WriteLine("Database and tables created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+
+    private void CreateDatabase(SqlConnection connection)
+    {
+        string createDbQuery = "CREATE DATABASE FlashcardApp";
+        SqlCommand createDbCommand = new SqlCommand(createDbQuery, connection);
+        createDbCommand.ExecuteNonQuery();
+    }
+
+    private void CreateTableStacks(SqlConnection connection)
+    {
+        string createTableQuery = @"
+            CREATE TABLE Stacks (
+                StackID INT PRIMARY KEY IDENTITY,
+                StackName NVARCHAR(50) NOT NULL
+            )";
+        SqlCommand createTableCommand = new SqlCommand(createTableQuery, connection);
+        createTableCommand.ExecuteNonQuery();
+    }
+
+    private void CreateTableFlashcards(SqlConnection connection)
+    {
+        string createTableQuery = @"
+            CREATE TABLE Flashcards (
+                FlashcardID INT PRIMARY KEY IDENTITY,
+                Question NVARCHAR(255) NOT NULL,
+                Answer NVARCHAR(255) NOT NULL,
+                StackID INT FOREIGN KEY REFERENCES Stacks(StackID)
+            )";
+        SqlCommand createTableCommand = new SqlCommand(createTableQuery, connection);
+        createTableCommand.ExecuteNonQuery();
+    }
+}
--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=(localdb)\localdbFlashcard;Integrated Security=True";
+        DatabaseManager dbManager = new DatabaseManager(connectionString);
+        dbManager.CreateDatabaseAndTables();
+    }
+}
